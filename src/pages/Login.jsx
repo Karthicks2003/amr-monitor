b8539199d73d8973e081ca83a67b91aaf9730b9f
Karthicks2003 <karthick.slsk@zohointern.com>
2026-06-26 09:54:42 +0530

    Connect frontend to ngrok backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,7 +32,7 @@ function Login() {
 
       const response =
         await axios.post(
-          "http://127.0.0.1:8000/login",
+          "https://occupant-perm-neurosis.ngrok-free.dev/login",
           {
             name,
             password,
